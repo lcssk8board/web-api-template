d1794abd59d700c37cd8e60602d41b60faec1c2e
lcssk8board <lucas.ce.bertin@gmail.com>
2015-11-16 10:21:42 -0200

    Commit inicial. Projeto de testes para aprendizado de webapi 2, asp.net identity, jwt e owin

---
+++ source/WebAuth/Controllers/OrdersController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebAuth.Infrastructure;
+
+namespace WebAuth.Controllers
+{
+    public class OrdersController : ApiController
+    {
+        [Authorize(Roles = "IncidentResolvers")]
+        [HttpPut]
+        [Route("refund/{orderId}")]
+        public IHttpActionResult RefundOrder([FromUri]string orderId)
+        {
+            return Ok();
+        }
+
+        [ClaimsAuthorization(ClaimType = "FTE", ClaimValue = "1")]
+        [Route("")]
+        public IHttpActionResult Get()
+        {
+            return Ok();
+        }
+    }
+}
